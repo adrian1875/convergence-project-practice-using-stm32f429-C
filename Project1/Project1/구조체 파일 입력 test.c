02b3d11df2eed999ae5a5dc97d625a9bbb935b6f
choi <choiyh1875@gmail.com>
2022-03-11 21:02:50 +0900

    구조체 배열을 이용한 파일 입력 유닛 테스트

---
+++ Project1/Project1/구조체 파일 입력 test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAX (1)
+
+typedef struct test_struct
+{
+	int num;
+	char name[10];
+	float mean;
+}test_st;
+
+int main(void)
+{
+	test_st st_arr[MAX] = { 0, };
+	int i;
+	FILE* test_file = fopen("test file.txt", "w");
+	
+	
+	for (i = 0; i < MAX; ++i) {
+		printf("%d번 학생의 학번입력 ", i + 1);
+		scanf("%d", &st_arr[i].num);
+
+		printf("%d번 학생의 이름입력 ", i + 1);
+		scanf("%s", st_arr[i].name);
+
+		printf("%d번 학생의 평균입력 ", i + 1);	
+		scanf("%f", &st_arr[i].mean);	
+	}
+
+	fprintf(test_file, "학번  \t    이름  \t   평균\n");
+	for (i = 0; i < MAX; ++i) {
+		fprintf(test_file, "%06d  \t    %s  \t   %.2f\n", st_arr[i].num, st_arr[i].name, st_arr[i].mean);
+	}
+
+	fclose(test_file);
+	return 0;
+}
+
